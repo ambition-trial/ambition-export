db603a273ce80ba8731ae49dd17b3684bd662c8d
erikvw <ew2789@gmail.com>
2019-11-07 23:44:34 -0600

    use get model utils

--- ambition_export/df_handlers.py
+++ ambition_export/df_handlers.py
@@ -1,5 +1,7 @@
 from edc_pdutils import CrfDfHandler as BaseCrfDfHandler
 from edc_pdutils import NonCrfDfHandler as BaseNonCrfDfHandler
+from edc_randomization.utils import get_randomizationlist_model_name
+from edc_visit_tracking.models import get_visit_tracking_model
 
 from .column_handlers import ColumnHandler
 
@@ -9,9 +11,9 @@ class CrfDfHandler(BaseCrfDfHandler):
     column_handler_cls = ColumnHandler
     na_value = "."
 
-    visit_tbl = "ambition_subject_subjectvisit"
+    visit_tbl = get_visit_tracking_model().replace(".", "_")
     enrollment_tbl = "ambition_screening_subjectscreening"
-    rando_tbl = "ambition_rando_randomizationlist"
+    rando_tbl = get_randomizationlist_model_name().replace(".", "_")
     sort_by = ["subject_identifier", "visit_datetime"]
 
 
